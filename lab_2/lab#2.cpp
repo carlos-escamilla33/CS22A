f7cbd9f9f145821e25ecb18d13f00083f8b89679
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-04-26 23:32:12 -0700

    completed logic of lab_2

--- lab_2/lab#2.cpp
+++ lab_2/lab#2.cpp
@@ -4,43 +4,53 @@
 */
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
+
+#define STATE_TAX (7.25 / 100)
 
 int main()
 {
     // Initializing variables
     string monthYear;
-    double cupertinoAmount;
-    double losAltosAmount;
-    double sunnyvaleAmount;
+    double cupertinoAmount = 23599.00;
+    double losAltosAmount = 4788.00;
+    double sunnyvaleAmount = 67123.00;
     double totalCollected;
     double countySalesTax;
+    double stateSalesTax;
     double sales;
+    double countyTaxRate = (1.75 / 100);
 
     // Input month and year
-    cout << "Enter the month and year:\t";
-    getline(cin, monthYear);
+    // cout << "Enter the month and year:\t";
+    // getline(cin, monthYear);
 
     // Input the total amount per store
-    cout << "Enter amount collected for Cupertino store:\t";
-    cin >> cupertinoAmount;
-    cout << "Enter amount collected for Los Altos store:\t";
-    cin >> losAltosAmount;
-    cout << "Enter amount collected for Sunnyvale store:\t";
-    cin >> sunnyvaleAmount;
+    // cout << "Enter amount collected for Cupertino store:\t";
+    // cin >> cupertinoAmount;
+    // cout << "Enter amount collected for Los Altos store:\t";
+    // cin >> losAltosAmount;
+    // cout << "Enter amount collected for Sunnyvale store:\t";
+    // cin >> sunnyvaleAmount;
 
     // Sales variable calculating total sales
-    sales = (cupertinoAmount + losAltosAmount + sunnyvaleAmount) / (1.0 + (7.25 / 100) + (1.75 / 100));
+    sales = (cupertinoAmount + losAltosAmount + sunnyvaleAmount) / (1.0 + STATE_TAX + (1.75 / 100.0));
     // Total Sales Collected
     totalCollected = cupertinoAmount + losAltosAmount + sunnyvaleAmount;
     // County Sales Tax
-    countySalesTax = (cupertinoAmount * (1.75 / 100)) + (losAltosAmount * (1.75 / 100)) + (sunnyvaleAmount * (1.75 / 100));
+    countySalesTax = sales * countyTaxRate;
+    stateSalesTax = sales * STATE_TAX;
+
 
     cout << monthYear << endl;
     cout << totalCollected << endl;
     cout << sales << endl;
+    cout << countySalesTax << endl;
+    cout << stateSalesTax << endl;
+    cout << countySalesTax + stateSalesTax << endl;
 
     return 0;
 }
