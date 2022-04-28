eebb2e35ac147cef0445ac07bbfbcdbefc862e26
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-04-27 22:02:35 -0700

    updated state and county tax in lab#2

--- lab_2/lab#2.cpp
+++ lab_2/lab#2.cpp
@@ -9,37 +9,40 @@
 
 using namespace std;
 
-#define STATE_TAX_RATE (7.25 / 100)
+#define STATE_TAX_RATE (6.75 / 100)
 
 int main()
 {
-    // Initializing variables
+    // Initializing location and tax variables
     string monthYear;
-    double cupertinoAmount = 23599.00;
-    double losAltosAmount = 4788.00;
-    double sunnyvaleAmount = 67123.00;
+    double cupertinoAmount;
+    double losAltosAmount;
+    double sunnyvaleAmount;
     double totalCollected;
     double countySalesTax;
     double stateSalesTax;
     double sales;
-    double countyTaxRate = (1.75 / 100);
+    double countyTaxRate = (2.75 / 100);
 
     // Input month and year
-    // cout << "Enter the month and year:\t";
-    // getline(cin, monthYear);
+    cout << "Enter the month and year:\t";
+    getline(cin, monthYear);
 
     // Input the total amount per store
-    // cout << "Enter amount collected for Cupertino store:\t";
-    // cin >> cupertinoAmount;
-    // cout << "Enter amount collected for Los Altos store:\t";
-    // cin >> losAltosAmount;
-    // cout << "Enter amount collected for Sunnyvale store:\t";
-    // cin >> sunnyvaleAmount;
+    cout << "Enter amount collected for Cupertino store:\t";
+    cin >> cupertinoAmount;
+    cout << "Enter amount collected for Los Altos store:\t";
+    cin >> losAltosAmount;
+    cout << "Enter amount collected for Sunnyvale store:\t";
+    cin >> sunnyvaleAmount;
 
-    // Sales variable calculating total sales
-    sales = (cupertinoAmount + losAltosAmount + sunnyvaleAmount) / (1.0 + STATE_TAX_RATE + countyTaxRate);
+
     // Total Sales Collected
     totalCollected = cupertinoAmount + losAltosAmount + sunnyvaleAmount;
+
+    // Sales variable calculating total sales
+    sales = (totalCollected) / (1.0 + STATE_TAX_RATE + countyTaxRate);
+
     // County Sales Tax
     countySalesTax = sales * countyTaxRate;
     stateSalesTax = sales * STATE_TAX_RATE;
