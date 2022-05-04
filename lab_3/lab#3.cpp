5f46f98c7e9ed3f111d7d538a1e4494f6b189df2
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-05-04 10:22:04 -0700

    setup inputs in lab#3

--- lab_3/lab#3.cpp
+++ lab_3/lab#3.cpp
@@ -1,14 +1,29 @@
 /*
     Carlos Rodriguez Escamilla
+    Crodriguezescamilla3@gmail.com
     Lab #3 - ETS Refresh 2022
 */
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main () {
     // Initalize variables
-    int tables, dellThins, dellAllInOne, keyboards, mouses;
+    int tables, dellThins, dellAllInOnes, keyboards, mouses;
 
-    
+    // Prompt user input
+    cout << "How many 24 x 72 inch tables ordered? ";
+    cin >> tables;
+    cout << "How many Dell thin clints ordered? ";
+    cin >> dellThins;
+    cout << "How many Dell All-in-One Thin Clients ordered? ";
+    cin >> dellAllInOnes;
+    cout << "How many Dell Keyboards ordered? ";
+    cin >> keyboards;
+    cout << "How many Dell mouses ordered? ";
+    cin >> mouses;
+
     return 0;
 }
