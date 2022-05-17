8964a71d83f744d73e45a40142dfa25fde72fb6a
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-05-16 21:28:40 -0700

    working on last function in lab#4

--- lab_4/lab#4.cpp
+++ lab_4/lab#4.cpp
@@ -6,30 +6,35 @@
 
 #include <iostream>
 #include <cctype>
+#include <iomanip>
 using namespace std;
 
 // Prototype
 double calcWaterAllotment(int numOfPeople);
 double calcLandscape(int monthOfYear, char residenceType);
+double calcBillTotal(double waterAllocated, double landscapeBudget);
+double calcBudgetTotal(double waterBudget, double cubicFeet);
 
 //////////////////////////////////////////////////
 
 int main() {
     // Initializing user household variables
     int numOfResidents = 4, monthOfYear = 3;
-    char residenceType = 'C';
-    double waterCalc, landScapeCalc;
+    char residenceType = 'S';
+    double waterCalc, landScapeCalc, billCalc, budgetTotalCalc, cubicFeet = 12.0;
 
     // Input user household info
     // cout << "Enter number of residents in household: ";
     // cin >> numOfResidents;
-    // cout << "Enter month as a number (1-12): ";
-    // cin >> monthOfYear;
     // cout << "Enter the type of residence: " << endl;
     // cout << "S - Single-family Residence" << endl;
     // cout << "C- Condo" << endl;
-    // cout << "A- Apartment" << endl;
+    // cout << "A- Apartment" << endl << ": ";
     // cin >> residenceType;
+    // cout << "Enter month as a number (1-12): ";
+    // cin >> monthOfYear;
+    // cout << "Enter Actual CCF (cubic feet) used: ";
+    // cin >> cubicFeet;
 
     if (residenceType != tolower(residenceType)) {
         residenceType = tolower(residenceType);
@@ -42,9 +47,15 @@ int main() {
     // Calculate Info
     waterCalc = calcWaterAllotment(numOfResidents);
     landScapeCalc = calcLandscape(monthOfYear, residenceType);
+    billCalc = calcBillTotal(waterCalc, landScapeCalc);
+    budgetTotalCalc = calcBudgetTotal(waterCalc, cubicFeet);
 
+
+    cout << setprecision(2) << fixed;
     cout << waterCalc << endl;
     cout << landScapeCalc << endl;
+    cout << billCalc << endl;
+    cout << budgetTotalCalc << endl;
 
     return 0;
 }
@@ -90,4 +101,53 @@ double calcLandscape(int month, char typeOfResidence) {
    }
 
     return landscapeBudget;
+} // calcLandScape
+
+//////////////////////////////////////////////////
+
+double calcBillTotal(double waterAllocated, double landscapeBudget) {
+    /*Pre: waterAllocated - water allocation for all people
+      landscapeBudget - landscape cost based on month and residence type
+      Post: output billTotal
+      Purpose: output water allocation plus landscape budget
+    */
+    double billTotal;
+
+    billTotal = waterAllocated + landscapeBudget;
+
+    return billTotal;
+} //calcBillTotal
+
+//////////////////////////////////////////////////
+
+double calcBudgetTotal(double waterBudget, double cubicFeet) {
+      /*Pre: billTotal - waterAllocated + landscapeBudget
+      cubicFeet - water volume measured in cubicFeet
+      Post: output budget total
+      Purpose: output cost based on cubic feet used
+    */
+    double budgetTotal;
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
