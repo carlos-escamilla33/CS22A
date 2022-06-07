a803c96c5c670c5fc77e2e02eaf5c76ff83a2596
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-06-06 20:38:09 -0700

    finished output function in lab#5

--- lab_5/lab#5.cpp
+++ lab_5/lab#5.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 // Prototype
 void userInfo(double &salesPrice, double &interestRate, int &yearsOfLoan);
-void calcMonthlyCosts(double &dwnPymt, double &amtOfLoan, double &mortagePymt, int yrsOfLoan);
-void calcDownLoanMortage(double &dwnPymt, double &amtOfLoan, double &mortagePymt);
-
+void calcMonthlyCosts(double &dwnPymt, double &amtOfLoan, double &mortagePymt, int yrsOfLoan, double salesPrice, double dwnPercentage, double &interestRate);
+void calcDownLoanMortage(double &dwnPymt, double &amtOfLoan, double &mortagePymt, double salesPrice, double dwnPercentage, int yrsOfLoan);
+void output(double mortagePymt, double dwnPymt, double amtLoan, double interestRate, double yrsLoan);
 
 // Preprocessor statements
 #define TAX_RATE 0.0125
@@ -23,21 +23,24 @@ void calcDownLoanMortage(double &dwnPymt, double &amtOfLoan, double &mortagePymt
 int main() {
     // Initializing variables
     const double downPymtPercentage = 0.20;
-    double sellingPrice, rateOfInterest, amountOfLoan, mortagePymt;
-    double downPayment;
+    double sellingPrice, rateOfInterest, amountOfLoan, mortagePymt, downPayment;
     int numYearsLoan;
 
-    // Call user info function and home calculations function
+    // Call user info function for home information
     userInfo(sellingPrice, rateOfInterest, numYearsLoan);
-    calcMonthlyCosts(downPayment, amountOfLoan, mortagePymt, numYearsLoan);
 
-    cout << "MORTAGE PAYMENT: " << mortagePymt << endl;
+    // Calc Montly expenses
+    calcMonthlyCosts(downPayment, amountOfLoan, mortagePymt, numYearsLoan, sellingPrice, downPymtPercentage, rateOfInterest);
+
+    // Output home info
+
+    output(mortagePymt, downPayment, amountOfLoan, rateOfInterest, numYearsLoan);
 
     return 0;
-}
+} //
 
 void userInfo(double &salePrice, double &interestRate, int &yearsOfLoan) {
-    /*Pre: 
+    /*Pre:
    salePrice - reference to sellingPrice
    interestRate - reference to rateOfInterest
    yearsOfLoan - reference to numYearsLoan
@@ -53,28 +56,71 @@ void userInfo(double &salePrice, double &interestRate, int &yearsOfLoan) {
     cin >> yearsOfLoan;
 
     return;
-}
+} // userInfo
 
-void calcMonthlyCosts(double &dwnPymt, double &amtOfLoan, double &mortagePymt, int yrsOfLoan) {
-     /*Pre: 
+void calcMonthlyCosts(double &dwnPymt, double &amtOfLoan, double &mortagePymt, int yrsOfLoan, double salesPrice, double dwnPercentage, double &rateOfInterest) {
+    /*Pre:
+   dwnPymt - reference to downPayment
+   amtOfLoan - reference to amountOfLoan
+   mortagePymt - reference to mortagePymt
+   yrsOfLoan - total number of years left to pay off home loan
+   salesPrice - total cost of home
+   dwnPercentage - percent paid based off the value of the home
+   Post: nothing
+   Purpose: Calculate home expenses, make all neccessary compuations
+  */
+
+    // Calaculate down payment, amount of loan, and mortage payment
+    calcDownLoanMortage(dwnPymt, amtOfLoan, mortagePymt, salesPrice, dwnPercentage, yrsOfLoan);
+
+    // Calculate interest rate
+    rateOfInterest = (TAX_RATE)*30;
+
+    return;
+
+} // calcMontlyCosts
+
+void calcDownLoanMortage(double &dwnPymt, double &amtOfLoan, double &mortagePymt, double salesPrice, double dwnPercentage, int yrsOfLoan) {
+    /*Pre:
     dwnPymt - reference to downPayment
     amtOfLoan - reference to amountOfLoan
     mortagePymt - reference to mortagePymt
-    yrsOfLoan - total number of years left to pay off home loan
+    salesPrice - total cost of home
     Post: nothing
-    Purpose: Calculate home down payment, amount of loan, and mortage payment
+    Purpose: Calculate down payment, amount of loan, mortage payment
    */
 
-//   Calaculate down payment, amount of loan, and mortage payment
-  calcDownLoanMortage(dwnPymt, amtOfLoan, mortagePymt);
+    // Calculate down payment on home
+    dwnPymt = (salesPrice * dwnPercentage);
 
-  
+    // Calculate amouont of loan after down payment
+    amtOfLoan = (salesPrice - dwnPymt);
 
-  return;
+    // Calculate mortage payment monthly
+    mortagePymt = (amtOfLoan * (TAX_RATE / 12) * pow(1 + (TAX_RATE / 12), (yrsOfLoan * 12))) / (pow((1 + (TAX_RATE / 12)), (yrsOfLoan * 12)) - 1);
 
-}
+    return;
 
+} // calcDownLoanMortage
 
-void calcDownLoanMortage(double &dwnPymt, double &amtOfLoan, double &mortagePymt) {
-    
-}
+void output(double mortagePymt, double dwnPymt, double amtLoan, double interestRate, double yrsLoan) {
+    /*Pre: dwnPymt - amount given based off the down payment percentage
+    amtLoan - amount of money left to pay after giving down payment
+    interestRate -
+    yrsLoan - years left to pay off home
+   Post: nothing
+   Purpose: output monthly cost of house and montly payment
+   */
+
+    // output
+    cout << setprecision(2) << fixed;
+
+    cout << "MORTAGE PAYMENT: $" << mortagePymt << endl;
+    cout << "DOWN PAYMENT: $" << downPymt << endl;
+    cout << "AMOUNT OF LOAN: $" << amtOfLoan << endl;
+    cout << "INTEREST RATE: " << interestRate << "%" << endl;
+    cout << "TAX RATE: " << (TAX_RATE * 100) << "%" << endl;
+    cout << "DURATION OF LOAN: " << yrsLoan << endl;
+
+    return;
+} // output
