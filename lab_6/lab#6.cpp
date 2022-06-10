7e639d3360098ca18b4d79f75e903ee3ec3e610c
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-06-09 20:48:05 -0700

    created lab#6 - cost of living

---
+++ lab_6/lab#6.cpp
@@ -0,0 +1,16 @@
+/*
+    Carlos Rodriguez Escamilla
+    Lab #6 - Cost of Living
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    // Initializing pointer to file
+    ifstream inFile;
+
+
+    return 0;
+}
