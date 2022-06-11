6f40aa48159f23b408d4fd0d21ea3d20d1c3619a
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-06-11 13:31:54 -0700

    working on function in lab#6

--- lab_6/lab#6.cpp
+++ lab_6/lab#6.cpp
@@ -7,40 +7,63 @@
 #include <fstream>
 #include <cstdlib>
 #include <string.h>
+#include <iomanip>
+#include <cstdio>
 using namespace std;
 
 // Protoype
-void calcCompositeIdx(string);
+string calcCompositeIdxs(double groceries, double housing, double utilities, double transportation, double health, double miscellaneous);
 
-int main() {
+int main()
+{
     // Declare variable
-    string word;
-
-    // Initializing pointers to file
+    string state, line;
+    double groceries, house, utilities, transportation, health, miscellaneous;
     ifstream fileIn;
+    char ch='\n';
 
     fileIn.open("Lab6_StateCOL.txt");
-    if(fileIn.fail()) {
-        cout << "ERROR IN READING FILE..." << endl;
+    if (!fileIn.is_open())
+    {
+        cout << "Could not open file..." << endl;
         exit(100);
     }
 
-    // Loop through words in file
+    cout << "Reading and printing file info..." << endl;
+    // fileIn >> state;
+    // fileIn >> groceries >> house >> utilities >> transportation >> health >> miscellaneous;
     while (!fileIn.eof()) {
-        cout << word << endl;
-        getline(fileIn, word);
+        if (getline(fileIn, state)) {
+            // fileIn >> line;
+            fileIn >> groceries >> house >> utilities >> transportation >> health >> miscellaneous;
+            fileIn.ignore(100,ch); 
+            cout << setprecision(1) << fixed;
+            cout << endl;
+            cout << "State Name:\t" << state << endl;
+            cout << groceries << " " << house << " " << utilities << " " << transportation << " " << health << " " << miscellaneous << endl;
+            // calcCompositeIdxs(groceries, house, utilities, transportation, health, miscellaneous);
+        }
     }
 
     // Closing file
     fileIn.close();
 
-
     return 0;
 }
 
-void calcCompositeIdx(string word) {
-    /*Pre
-    Post
-    
+string calcCompositeIdxs(double groceries, double housing, double utilities, double transportation, double health, double miscellaneous)
+{
+    /*Pre:fileLineInfo - line of the file
+    Post: nothing
+    Purpose: return the calculatedhousing of each state;
+
     */
+    string calculatedStateIdxs = "idfdsaf";
+
+    // for (int i = 0; i <housing.length(); i++)
+    // {
+        // cout << groceries << housing << utilities << transportation << health << miscellaneous << endl;
+    // }
+
+    return calculatedStateIdxs;
 }
