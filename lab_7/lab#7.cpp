b2c40482e8451e993d70715858545385def05c2b
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-06-18 19:25:25 -0700

    finished input function in lab#7

--- lab_7/lab#7.cpp
+++ lab_7/lab#7.cpp
@@ -8,33 +8,72 @@
 // #include <string.h>
 using namespace std;
 
-void inputUniversityInfo();
+void inputUniversityData(string[], string[], string[], int[], int[], double[], double[], ifstream &);
 
 int main() {
     // Initialize school info variables
-    string universities[1000], state[1000], city[1000];
-    int yrlyTutition[1000], enrollment[1000];
-    double avgFreshmanRetention[1000], double gradsInSixYrs[1000];
+    int arrSize = 1000;
+    string universities[arrSize], states[arrSize], cities[arrSize];
+    int yrlyTuition[arrSize], enrollments[arrSize];
+    double avgFreshmanRetention[arrSize], gradsInSixYrs[arrSize];
 
     // Initialize Reference to file
     ifstream fileIn;
     ofstream fileOut;
 
     // Open fileIn and fileOut
-    fileIn.open("/universities.txt");
+    fileIn.open("universities.txt");
     // fileOut.open("sample")
 
     // Checking fileIn for corrupt/unusable data
-    if (!fileIn.is_open()) {
+    if (!fileIn.is_open())
+    {
         cout << "Error reading file..." << endl;
         exit(1);
     }
 
-    // Run loop till end of fileIn
+    // Get univeristy data
+    inputUniversityData(universities, states, cities, yrlyTuition, enrollments, avgFreshmanRetention, gradsInSixYrs, fileIn);
 
+    // Close files
+    fileIn.close();
 
     // Output
 
-
     return 0;
+}
+
+void inputUniversityData(string universities[], string states[], string cities[], int yrlyTuition[],
+                         int enrollments[], double avgFreshmanRetention[], double gradsInSixYrs[], ifstream &fileIn) {
+    /*
+        Pre: universities - reference to universities array
+        state - reference to states array
+        cities - reference to cities array
+        yrlyTutition - reference to yrlyTuition array
+        enrollments - reference to enrollment array
+        avgFreshmanRetention - reference to avgFreshmanRetention
+        gradsInSixYrs - reference to gradsInSixYrs
+        fileIn - reference to fileIn
+        Post: nothing
+        Purpose:
+    */
+    double placeHolder;
+    int idx = 0;
+
+    while (!fileIn.eof()) {
+        // Read line of files
+        getline(fileIn, universities[idx]);
+        fileIn >> states[idx];
+        getline(fileIn, cities[idx]);
+        fileIn >> yrlyTuition[idx] >> enrollments[idx] >> placeHolder;
+        fileIn >> avgFreshmanRetention[idx] >> gradsInSixYrs[idx];
+
+        // Skips next 100 input characters, until new line is read
+        fileIn.ignore(100, '\n');
+
+        // Increment the index by 1
+        idx++;
+    }
+
+    return;
 }
